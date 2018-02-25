311dec6ab6d41a5badf8aa7c4d3c94c44ff805ab
Meron Kebede <kebedemeron905@gmail.com>
2018-02-24 19:10:42 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,26 @@
 
-console.log("Up and Running!");
 
 var cards = [ "queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped" + " " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	// Code to be executed if condition1 is true
-
-}  if (cardsInPlay[0] === cardsInPlay[2]){
-   alert("You found a match!") 
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
 
    } else {
-   	alert("Sorry, try again.")
+   	alert("Sorry, try again.");
    }
+};
+
+var flipCard = function(cardId) {
+
+cardsInPlay.push(cards[cardId]);
+console.log("user flipped" + " " + cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+	flipCard(2);
+
+
+
+
